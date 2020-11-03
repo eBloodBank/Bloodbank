04f3f49a91de86cdb2428ccdb146de59978dc974
utkarshaditya01 <utkarshaditya01@gmail.com>
2020-11-03 19:59:26 +0530

    Login,Logout backend

--- Project/bloodbank/bloodbank/urls.py
+++ Project/bloodbank/bloodbank/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from bloodbank.user import views as account_views
+from user import views as account_views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('', include('website_pages.urls')),
     path('admin/', admin.site.urls),
-    path('register/',account_views.register_view,name='register'),
+    path('register/', account_views.register_view, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
 ]
