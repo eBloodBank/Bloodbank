42aecb250577ed662afbf92334829a68bf0f24a8
utkarshaditya01 <utkarshaditya01@gmail.com>
2020-11-03 18:21:20 +0530

    Created routes for home and about us

--- Project/bloodbank/bloodbank/urls.py
+++ Project/bloodbank/bloodbank/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from user import views as account_views
 
 urlpatterns = [
-    path('', include('user.urls')),
+    path('', include('website_pages.urls')),
     path('admin/', admin.site.urls),
+    path('register/',account_views.register_view,name='register'),
 ]
