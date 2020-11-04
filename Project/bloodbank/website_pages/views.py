d58e565bfff5e6f3f4002e8f84def03e246b82cc
utkarshaditya01 <utkarshaditya01@gmail.com>
2020-11-05 01:02:11 +0530

    Update, Delete Blood Bank and modified pages.

--- Project/bloodbank/website_pages/views.py
+++ Project/bloodbank/website_pages/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from .models import BloodBank, BloodPacket
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.forms.models import inlineformset_factory
 from django.urls import reverse
-
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 def home(request):
     return render(request,'website_pages/home.html')
@@ -20,7 +20,7 @@ class BloodBankDetailView(DetailView):
     model = BloodBank
 
 BloodPacketFormset = inlineformset_factory(BloodBank, BloodPacket, fields=('packetID', 'bloodGroup', 'expiryDate', 'quantity'))
-class BloodBankCreateView(CreateView):
+class BloodBankCreateView(LoginRequiredMixin, CreateView):
     model = BloodBank    
     fields = ['name', 'district', 'city', 'state', 'category', 'contactNo', 'email', 'PostalAddress' ]
 
@@ -49,4 +49,52 @@ class BloodBankCreateView(CreateView):
 
     def get_success_url(self):
         return reverse('bloodbank-list')
+
+class BloodBankUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = BloodBank
+    fields = ['name', 'district', 'city', 'state', 'category', 'contactNo', 'email', 'PostalAddress' ]
+
+    def get_context_data(self, **kwargs):
+        # we need to overwrite get_context_data
+        # to make sure that our formset is rendered.
+        # the difference with CreateView is that
+        # on this view we pass instance argument
+        # to the formset because we already have
+        # the instance created
+        data = super().get_context_data(**kwargs)
+        if self.request.POST:
+            data["BloodPacketz"] = BloodPacketFormset(self.request.POST, instance=self.object)
+        else:
+            data["BloodPacketz"] = BloodPacketFormset(instance=self.object)
+        return data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        BloodPacketz = context["BloodPacketz"]
+        self.object = form.save()
+        if BloodPacketz.is_valid():
+            BloodPacketz.instance = self.object
+            BloodPacketz.save()
+        setz = BloodPacket.objects.filter(Blood_bank = form.instance)
+        form.instance.BloodPackets.add(*setz)
+        return super().form_valid(form)
+    
+    def test_func(self):
+        BloodBankz = self.get_object()
+        if self.request.user == BloodBankz.BloodBankAdmin:
+            return True
+        return False
+
+    def get_success_url(self):
+        return reverse('bloodbank-list')
+
+class BloodBankDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = BloodBank
+    success_url = '/'
+    
+    def test_func(self):
+        BloodBankz = self.get_object()
+        if self.request.user == BloodBankz.BloodBankAdmin:
+            return True
+        return False
 
