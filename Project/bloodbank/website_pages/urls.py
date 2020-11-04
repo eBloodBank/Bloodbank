339bd813b24d2c1e4ee76fdbefd7cb1046bd03aa
utkarshaditya01 <utkarshaditya01@gmail.com>
2020-11-04 21:43:18 +0530

    Added BloodBank Model, BloodPacket Model and views

--- Project/bloodbank/website_pages/urls.py
+++ Project/bloodbank/website_pages/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path,include
 from . import views
+from .views import BloodBankListView, BloodBankDetailView, BloodBankCreateView
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('bloodbanks/', BloodBankListView.as_view(), name='bloodbank-list'),
+    path('bloodbank/new/', BloodBankCreateView.as_view(), name='bloodbank-create'),
+    path('bloodbank/<int:pk>', BloodBankDetailView.as_view(), name='bloodbank-detail'),
 ]
