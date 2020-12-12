b37b0ef59bbce7aa0103348dbbca87e406945958
utkarshaditya01 <utkarshaditya01@gmail.com>
2020-12-12 14:43:50 +0530

    Finalized pages and testing

--- Project/bloodbank/website_pages/tests/test_models.py
+++ Project/bloodbank/website_pages/tests/test_models.py
@@ -11,3 +11,27 @@ class TestModels:
     def test_bloodpacket_quantity_is_invalid(self):
         bloodpacketz = mixer.blend('website_pages.BloodPacket', quantity=0)
         assert bloodpacketz.quantity_check == False
+    
+    def test_bb_str(self):
+        bloodbankz = mixer.blend('website_pages.BloodBank',name = "testing")
+        assert str(bloodbankz)== bloodbankz.name
+    
+    def test_bp_str(self):
+        bloodpacketz = mixer.blend('website_pages.BloodPacket',packetID = '1234')
+        assert str(bloodpacketz)==bloodpacketz.packetID
+    
+    def test_be_str(self):
+        bloodcampz = mixer.blend('website_pages.BloodDonationEvent',campName = "testing")
+        assert str(bloodcampz)== bloodcampz.campName
+    
+    def test_order_str(self):
+        orderz = mixer.blend('website_pages.Order',packetID = '1234')
+        assert str(orderz)== orderz.packetID
+
+    def test_usr_short_name(self):
+        usez = mixer.blend('user.User', first_name="utkarsh", last_name="aditya")
+        assert usez.get_short_name() == "utkarsh"
+
+    def test_usr_full_name(self):
+        usez = mixer.blend('user.User', first_name="utkarsh", last_name="aditya")
+        assert usez.get_full_name() == "utkarsh aditya"
--- Project/bloodbank/website_pages/tests/test_urls.py
+++ Project/bloodbank/website_pages/tests/test_urls.py
@@ -2,6 +2,38 @@ from django.urls import reverse, resolve
 
 class TestUrls:
 
-    def test_detail_url(self):
+    def test_detail_url_bb(self):
         path = reverse('bloodbank-detail', kwargs={'pk': 39})
         assert resolve(path).view_name == 'bloodbank-detail'
+
+    def test_detail_url_bp(self):
+        path = reverse('bloodpackets-detail', kwargs={'pk': 39})
+        assert resolve(path).view_name == 'bloodpackets-detail'
+    
+    def test_detail_url_bd(self):
+        path = reverse('donations-detail', kwargs={'pk': 3})
+        assert resolve(path).view_name == 'donations-detail'
+
+    def test_detail_url_bb_update(self):
+        path = reverse('bloodbanks-update', kwargs={'pk': 39})
+        assert resolve(path).view_name == 'bloodbanks-update'
+    
+    def test_detail_url_bb_delete(self):
+        path = reverse('bloodbanks-delete', kwargs={'pk': 39})
+        assert resolve(path).view_name == 'bloodbanks-delete'
+    
+    def test_detail_url_bb_create(self):
+        path = reverse('bloodbank-create')
+        assert resolve(path).view_name == 'bloodbank-create'
+
+    def test_detail_url_home(self):
+        path = reverse('home')
+        assert resolve(path).view_name == 'home'
+
+    def test_detail_url_about(self):
+        path = reverse('about')
+        assert resolve(path).view_name == 'about'
+
+    def test_detail_url_complete(self):
+        path = reverse('complete')
+        assert resolve(path).view_name == 'complete'
